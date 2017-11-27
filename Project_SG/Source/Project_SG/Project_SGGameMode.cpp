090b46cdef487c6cc08df36515e8b663e92c45ff
ChrisCurreri <ccurreri95@gmail.com>
2017-11-26 22:54:07 -0500

    Commit project files
    I created a project in unity and I am commiting the files to the repo.

---
+++ Project_SG/Source/Project_SG/Project_SGGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "Project_SGGameMode.h"
+#include "Project_SGCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AProject_SGGameMode::AProject_SGGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
