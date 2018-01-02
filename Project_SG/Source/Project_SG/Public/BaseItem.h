73d99b31899ca73aeb79e96c39d554f7b9188b62
ChrisCurreri <ccurreri95@gmail.com>
2018-01-01 22:23:44 -0500

    BaseInteractable and BaseItem
    Created Classes that will cover the basic behavior of interactable items. Plan to code BaseItem for the katana.

---
+++ Project_SG/Source/Project_SG/Public/BaseItem.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseInteractable.h"
+#include "BaseItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_SG_API ABaseItem : public ABaseInteractable
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
